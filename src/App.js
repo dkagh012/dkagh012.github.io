fe18ed2e4b512d4ef5d30c62c96fa9a8e2c14ada
dkagh012 <qowjdxo00@gmail.com>
2022-11-29 23:16:56 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import Home from './component/Home';
-import Wrapper from './component/Wrapper';
-import Skils from './component/Skils';
-import Todo from './component/Todo';
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
@@ -16,17 +13,6 @@ function App() {
           <Home/>
         </Route>
 
-        <Route exact path="/Wrapper">
-          <Wrapper/>
-        </Route>
-
-        <Route exact path="/Skils">
-          <Skils/>
-        </Route>
-        
-        <Route exact path="/todo">
-          <Todo/>
-        </Route>
       
 
         
